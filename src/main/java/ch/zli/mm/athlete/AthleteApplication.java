46f84981444f0c8020478659053539b3a9a03c9a
Mi2cha <mmischa09@gmail.com>
2021-07-15 09:21:46 +0200

    git added project

---
+++ src/main/java/ch/zli/mm/athlete/AthleteApplication.java
@@ -0,0 +1,13 @@
+package ch.zli.mm.athlete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthleteApplication.class, args);
+	}
+
+}
